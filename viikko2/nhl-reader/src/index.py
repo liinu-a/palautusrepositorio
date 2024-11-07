ad6c99c59b08ac1426946d0885a9cfda5ac18016
liinu-a <liinu.arola@helsinki.fi>
2024-11-07 15:56:36 +0200

    Nhl-reader refactoring

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,23 +1,13 @@
-import requests
-from player import Player
+from player import PlayerReader, PlayerStats
+
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    fin_players = sorted(
-            filter(lambda player: (player.nationality == 'FIN'), players),
-        key=lambda player: player.points, reverse=True)
-    
-    print("Players from FIN:\n")
-
-    for player in fin_players:
+    for player in players:
         print(player)
 
 
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,3 +1,5 @@
+import requests
+
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
@@ -6,8 +8,25 @@ class Player:
         self.goals = dict['goals']
         self.team = dict['team']
         self.games = dict['games']
-        self.id = dict['id']
         self.points = self.assists + self.goals
-    
+
     def __str__(self):
         return f'{self.name:20} {self.team:4} {self.goals:2} + {self.assists:2} = {self.points}'
+    
+
+class PlayerReader:
+    def __init__(self, url):
+        self.response = requests.get(url).json()
+
+    def get_players(self):
+        return [Player(player_dict) for player_dict in self.response]
+
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nat):
+        return sorted(filter(
+                lambda player: (player.nationality == nat), self.players
+            ), key=lambda player: player.points, reverse=True)
