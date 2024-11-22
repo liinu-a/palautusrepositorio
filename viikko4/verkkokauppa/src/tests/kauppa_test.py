232a2a181806fde0abd103df077f12e41fbfac18
liinu-a <liinu.arola@helsinki.fi>
2024-11-22 21:59:13 +0200

    Viikko4 tehtävä 3

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -132,4 +132,3 @@ class TestKauppa(unittest.TestCase):
         kauppa.tilimaksu("pekka", "12345")
 
         self.pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", "33333-44455", 5)
-
