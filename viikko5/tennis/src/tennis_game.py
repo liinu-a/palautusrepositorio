3ab29dbb686473c09aa6d4cdfdabb33a5590e714
liinu-a <liinu.arola@helsinki.fi>
2024-12-01 14:21:58 +0200

    tennis_game.py refaktorointi

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -4,52 +4,62 @@ class TennisGame:
         self.player2_name = player2_name
         self.player1_score = 0
         self.player2_score = 0
+        self.score = ""
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.player1_score = self.player1_score + 1
+        if player_name == self.player1_name:
+            self.player1_score += 1
+        elif player_name == self.player2_name:
+            self.player2_score += 1
+
+    def get_score_name(self, score_num):
+        match score_num:
+            case 0:
+                return "Love"
+            case 1:
+                return "Fifteen"
+            case 2:
+                return "Thirty"
+            case 3:
+                return "Forty"
+
+    def equal_score(self):
+        score_num = self.player1_score
+        if score_num >= 3:
+            self.score = "Deuce"
+            return
+        self.score = self.get_score_name(score_num) + "-All"
+
+    def winner(self, point_difference):
+        if point_difference >= 2:
+            self.score = f"Win for {self.player1_name}"
         else:
-            self.player2_score = self.player2_score + 1
+            self.score = f"Win for {self.player2_name}"
+
+    def score_over_40(self):
+        point_difference = self.player1_score - self.player2_score
+
+        match point_difference:
+            case 1:
+                self.score = f"Advantage {self.player1_name}"
+            case -1:
+                self.score = f"Advantage {self.player2_name}"
+            case _:
+                self.winner(point_difference)
+
+    def scores_under_40(self):
+        score_name_player1 = self.get_score_name(self.player1_score)
+        score_name_player2 = self.get_score_name(self.player2_score)
+        self.score = score_name_player1 + "-" + score_name_player2
+
+    def set_score(self):
+        if self.player1_score == self.player2_score:
+            self.equal_score()
+        elif self.player1_score >= 4 or self.player2_score >= 4:
+            self.score_over_40()
+        else:
+            self.scores_under_40()
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
-        if self.player1_score == self.player2_score:
-            if self.player1_score == 0:
-                score = "Love-All"
-            elif self.player1_score == 1:
-                score = "Fifteen-All"
-            elif self.player1_score == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.player1_score >= 4 or self.player2_score >= 4:
-            minus_result = self.player1_score - self. player2_score
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.player1_score
-                else:
-                    score = score + "-"
-                    temp_score = self.player2_score
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+        self.set_score()
+        return self.score
