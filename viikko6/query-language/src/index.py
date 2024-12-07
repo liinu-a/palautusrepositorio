da5c14da419cccafe571210494c0d4ad08f4def9
liinu-a <liinu.arola@helsinki.fi>
2024-12-07 23:41:17 +0200

    Parannettu kyselykieli, osa 1

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,26 +1,19 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, Or, HasAtLeast, PlaysIn, Not, HasFewerThan, All
+from query_builder import QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(70, "points"),
-        Or(
-            PlaysIn("NYR"),
-            PlaysIn("FLA"),
-            PlaysIn("BOS")
-        )
-    )
+    query = QueryBuilder()
+    matcher = query.build()
+
+    matcher = query.plays_in("NYR").has_at_least(10, "goals").has_fewer_than(20, "goals").build()
 
     for player in stats.matches(matcher):
         print(player)
-
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
 
 
 if __name__ == "__main__":
---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,23 @@
+from matchers import And, All, PlaysIn, HasAtLeast, HasFewerThan, Not
+
+class QueryBuilder:
+    def __init__(self, query = None):
+        if query is None:
+            query = [All()]
+        self.query = query
+
+    def plays_in(self, team):
+        return QueryBuilder(self.query + [(PlaysIn(team))])
+
+    def has_at_least(self, value, attr):
+        return QueryBuilder(self.query + [HasAtLeast(value, attr)])
+        
+    def has_fewer_than(self, value, attr):
+        return QueryBuilder(self.query + [HasFewerThan(value, attr)])
+        
+    def negation(self, cond):
+        return QueryBuilder(self.query + [Not(cond)])
+
+    def build(self):
+        return And(*self.query)
+    
